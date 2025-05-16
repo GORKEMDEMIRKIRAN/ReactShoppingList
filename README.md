2900fbb232de09fba8ab256ed3cc852e9603079d
GÖRKEM-DEMİRKIRAN <gorkemgorkem1998@gmail.com>
2025-05-16 11:32:08 +0300

    Project puslish link added

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 
 There is a shopping cart project.
 
-
-
+## Project Site Link
+<a href="https://shoppingcardlist.netlify.app/">
--- src/data/data.js
+++ src/data/data.js
@@ -1,10 +1,12 @@
 
 
 export const dataArray=[
+    
     {id:1,title:'eggs',quantity:3,completed:true},
     {id:2,title:'olive',quantity:2,completed:false},
     {id:3,title:'cheese',quantity:7,completed:true},
     {id:4,title:'bread',quantity:5,completed:false},
     {id:5,title:'strawberry',quantity:2,completed:true},
     {id:6,title:'banger',quantity:2,completed:false},
+
 ];
